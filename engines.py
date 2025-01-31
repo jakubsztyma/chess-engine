31df80a23ab19f91ad40dde2fd62a17faa63be04
Jakub Sztyma <jakub.sztyma@revolut.com>
2025-01-31 12:33:15 +0100

    More generic MinMax

--- engines.py
+++ engines.py
@@ -20,30 +20,36 @@ class RandomEngine(BaseEngine):
 
 class MinMaxEngine(BaseEngine): # TODO improve the code
     def play(self, board: Board, *args, **kwargs):
-        best_move = next(iter(board.legal_moves))
-        best_result = -9999999 # TODO
-
-        for move_white in board.legal_moves:
-            board.push(move_white)
-            if board.is_checkmate():
-                board.pop()
-                best_move = move_white
-                best_result = 999999
-                break
-
-            for move_black in board.legal_moves:
-                board.push(move_black)
-
-                result = sum(self.evaluale_piece(c) for c in board.fen())
-
-                if result > best_result:
-                    best_move = move_white
-                    best_result = result
-
-                board.pop()
-            board.pop()
+        best_move, evaluation = self.find_move(board, depth=3, is_white=board.turn)
 
         return PlayResult(best_move, None)
+
+    def find_move(self, board: Board, depth: int, is_white: bool):
+        if depth == 0:
+            return None, sum(self.evaluale_piece(c) for c in board.fen())
+
+        if len(list(board.legal_moves)) == 0: # FIXME improve
+            return None, 0 # Draw
+
+        best_move = next(iter(board.legal_moves))
+        best_result = -9999999 if is_white else 9999999 # TODO
+
+        for move in board.legal_moves:
+            board.push(move)
+            if board.is_checkmate(): # TODO improve the code
+                best_move = move
+                board.pop()
+                break
+
+            _, result = self.find_move(board, depth=depth-1, is_white=board.turn)
+
+            if (is_white and result > best_result) or (not is_white and result < best_result):
+                best_move = move
+                best_result = result
+
+            board.pop()
+
+        return best_move, best_result
 
     def evaluale_piece(self, piece: str) -> float:
         piece_lower = piece.lower()
